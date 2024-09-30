eb1e385784d3c385563e464a2ea4bb5c7a025ac6
9bn1dyp <9bn1dyp@gmail.com>
2024-10-01 00:37:57 +0100

    Added CallToAction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { ProductShowcase } from "@/sections/ProductShowcase";
 import { Features } from '@/sections/Features'
 import { Pricing } from "@/sections/Pricing";
 import { Testimonials } from "@/sections/Testimonials";
+import { CallToAction } from "@/sections/CallToAction";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Features />
       <Pricing />
       <Testimonials />
+      <CallToAction />
     </>
   );
 }
